15c2bbc9470dfc292a07d9825d0426067a8eae3e
anvesh <anveshmishra25@gmail.com>
2023-05-10 00:48:53 +0530

    wiP: sign up page

--- lib/bindings/bindings.dart
+++ lib/bindings/bindings.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
 import 'package:thikedaardotcom/screens/login_screen/controller/login_controller.dart';
+import 'package:thikedaardotcom/screens/sign_up_screen/controller/sign_up_controller.dart';
 
 class GlobalBindings extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
     Get.put<LoginController>(LoginController(), permanent: true);
+    Get.put<SignUpController>(SignUpController(), permanent: true);
   }
 }
